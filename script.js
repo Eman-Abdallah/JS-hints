6ee947fa36ca84e8d4a45e077d1ea33897389aa7
Eman Abdallah <emanshaltoot623@gmail.com>
2023-09-07 17:26:19 +0300

    formate docs

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-let array=[10,21,13,40,5,6,7,8,9,10];
+let array = [10, 21, 13, 40, 5, 6, 7, 8, 9, 10];
 //sort array
-console.log(array.sort((a,b)=> a-b));
+console.log(array.sort((a, b) => a - b));
 //unique array
-console.log(array.filter((a,index)=>array.indexOf(a)===index));
+console.log(array.filter((a, index) => array.indexOf(a) === index));
 //duplicate values
-console.log(array.filter((a,index)=>array.indexOf(a)!==index));
+console.log(array.filter((a, index) => array.indexOf(a) !== index));
 //sum of an array
-console.log(array.reduce((acc,val)=>acc+val));
+console.log(array.reduce((acc, val) => acc + val));
 //git max of array
 console.log(Math.max(...array));
 //get min of array
 console.log(Math.min(...array));
 //git odd number of values
-console.log(array.filter((el)=>el%2!==0));
+console.log(array.filter((el) => el % 2 !== 0));
 //get even number of values
-console.log(array.filter((el)=>el%2===0));
+console.log(array.filter((el) => el % 2 === 0));
 
 
 // Capitalize first letter
@@ -26,21 +26,21 @@ console.log(array.filter((el)=>el%2===0));
  * capitalize.
  */
 const Capitalize = (str) => str.charAt(0
-    ).toUpperCase() + str.slice(1);
-    
-    Capitalize("follow for more");
-    console.log(Capitalize("follow for more"));
+).toUpperCase() + str.slice(1);
 
-    //Clear all cookies
+Capitalize("follow for more");
+console.log(Capitalize("follow for more"));
+
+//Clear all cookies
 // const clearCookies=document.cookie.split(";").forEach((cookie) =>(document.cookie=cookie.replace(/^ +/,"")
 // .replace(/=.*/, `“e*;expires=${new Date(0).toUTCString()};path=/"`)));
 
 // Remove duplicate from array
-const remove = (arr)=> [...new Set (arr)];
-console.log(remove([1,3,5,8,1,7,9,2,4,5]));
+const remove = (arr) => [...new Set(arr)];
+console.log(remove([1, 3, 5, 8, 1, 7, 9, 2, 4, 5]));
 
 // Scroll to tob
-const goToTob=() => window.scrollTo(0,0);
+const goToTob = () => window.scrollTo(0, 0);
 
 // Convert RGB to Hex
 
@@ -54,111 +54,112 @@ const goToTob=() => window.scrollTo(0,0);
  * intensity of the blue color in the resulting hex code.
  */
 const rgbToHex = (r, g, b) =>
-"#" + ((1<<24)+(r<<16)+(g<<8)+b).toString(16).slice(1);
-console.log(rgbToHex(24,0,255));
+    "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+console.log(rgbToHex(24, 0, 255));
 
 // find a day of a year
 
-const dayOfYear = (date) => Math.floor((date- new Date(date.getFullYear(),0,0))/1000/60/60/24);
+const dayOfYear = (date) => Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
 console.log(dayOfYear(new Date()));
 
 //hide the credit card number 
-const creditNumber="1234598732584524";
-let last4Number=creditNumber.slice(-4);
-let maskedNumber = last4Number.padStart(creditNumber.length,"*");
-console. log(maskedNumber) ;
+const creditNumber = "1234598732584524";
+let last4Number = creditNumber.slice(-4);
+let maskedNumber = last4Number.padStart(creditNumber.length, "*");
+console.log(maskedNumber);
 
-let x=[1,2,3];
-let y= x;
+let x = [1, 2, 3];
+let y = x;
 y.pop();
 console.log(x); // [1,2]
 
 
-    // Generate a list of unique nested objects and arrays
-    
-    const data = [...Array(1000).keys()].map(num => {
-        return { [num]: [{ num }] }}
-    )
-    console.log(data);
-        const deepClone = structuredClone(data)
-        console.log(deepClone);
-        
-        // const deepClone = JSON.parse(JSON.stringify(data) )
+// Generate a list of unique nested objects and arrays
+
+const data = [...Array(1000).keys()].map(num => {
+    return { [num]: [{ num }] }
+}
+)
+console.log(data);
+const deepClone = structuredClone(data)
+console.log(deepClone);
+
+// const deepClone = JSON.parse(JSON.stringify(data) )
 
 
 
 
-const employee={
-    name:'Eman',
-    Age:34,
-    salary:5000
+const employee = {
+    name: 'Eman',
+    Age: 34,
+    salary: 5000
 }
 // in ====> check if the key is already in the  object returned true or false
-const isSalary='salary' in employee;
+const isSalary = 'salary' in employee;
 console.log(isSalary); //true
-const isGender='gender' in employee;
+const isGender = 'gender' in employee;
 console.log(isGender);  //false
 
 // conditional add properties to an object
-includeSalary=true;
-const employee1={
-    name:'Eman',
-    Age:34,
-    ...(includeSalary && {salary:5000})
+includeSalary = true;
+const employee1 = {
+    name: 'Eman',
+    Age: 34,
+    ...(includeSalary && { salary: 5000 })
 }
 console.log(employee1);
 
 // includes() method detect if value is found in array or not
-const rgbColor=['red','green','blue'];
-const isRGP=(color)=>{
+const rgbColor = ['red', 'green', 'blue'];
+const isRGP = (color) => {
     return rgbColor.includes(color);
 }
 console.log(isRGP('yellow'));
 // isArray() method detect if value is an array
-console.log('Is this an array? '+ Array.isArray(rgbColor));
+console.log('Is this an array? ' + Array.isArray(rgbColor));
 // using spreed operator to make copy of array or object
-const rgbColorCopy=[...(rgbColor)];
-const newEmployee={...(employee1)};
+const rgbColorCopy = [...(rgbColor)];
+const newEmployee = { ...(employee1) };
 console.log(rgbColorCopy);
 console.log(newEmployee);
 // removing properties from object without affecting original object
-const {salary,...newEmployees} = newEmployee;
+const { salary, ...newEmployees } = newEmployee;
 console.log(newEmployees);
 
 //falsy value in JS is undefined, null, "", NaN, false, 0
 // Filtering array from falsy value by using filter(boolean)
-const mixed=["",null,false,true,"Eman",0,undefined,NaN,500];
+const mixed = ["", null, false, true, "Eman", 0, undefined, NaN, 500];
 console.log(mixed.filter(Boolean));
 console.log(mixed);
 
 // find()  ||| some()
-const assets =[
-    {id:1,title:'v-1',type:'video'},
-    {id:1,title:'v-1',type:'video'},
-    {id:1,title:'v-1',type:'audio'},
+const assets = [
+    { id: 1, title: 'v-1', type: 'video' },
+    { id: 1, title: 'v-1', type: 'video' },
+    { id: 1, title: 'v-1', type: 'audio' },
 ]
-console.log(assets.find((asset)=>asset.type==="video"));  //{id: 1, title: 'v-1', type: 'video'}
-console.log(assets.some((asset)=>asset.type==="video"));  // true
+console.log(assets.find((asset) => asset.type === "video"));  //{id: 1, title: 'v-1', type: 'video'}
+console.log(assets.some((asset) => asset.type === "video"));  // true
 
 //using numeric separator (_) in large numbers
-const large= 450_000_000; // === 450000000 === 45e9
+const large = 450_000_000; // === 450000000 === 45e9
 
 //Object destructuring frm an array  ** property name corresponding to index of item in array **
-const colors=['#000000', '#ffffff','#00ff00','#ff0000','#0000ff'];
-const {0:firstColor,4:lastColor}=colors;
+const colors = ['#000000', '#ffffff', '#00ff00', '#ff0000', '#0000ff'];
+const { 0: firstColor, 4: lastColor } = colors;
 console.log(firstColor);  //#000000
 console.log(lastColor);  //#0000ff
 //skipping element of an array by using empty and ,
-const[,,...newColors]=colors;
-console.log(newColors ); //['#00ff00', '#ff0000', '#0000ff']
+const [, , ...newColors] = colors;
+console.log(newColors); //['#00ff00', '#ff0000', '#0000ff']
 
 // JSON.stringify() transfer JS object to JSON object
-const json=JSON.stringify(employee1);
+const json = JSON.stringify(employee1);
 console.log(json); // {"name":"Eman","Age":34,"salary":5000}
 
 // when passing anther paremter to  JSON.stringify() work as a filter to this object only show property on this filter
-const filters =["name","Age"];
-const jsonFiltered=JSON.stringify(employee1, filters);
+const filters = ["name", "Age"];
+const jsonFiltered = JSON.stringify(employee1, filters);
 console.log(jsonFiltered); //{"name":"Eman","Age":34}
 
 
@@ -166,53 +167,53 @@ function /* The `formatCurrency` function is a utility function that takes in th
 `amount`, `currencyCode`, and `local`. It uses the `Intl.NumberFormat` object to format the
 `amount` parameter as a currency value based on the specified `currencyCode` and `local`
 parameters. */
-formatCurrency(amount,currencyCode,local='en-Us'){
-    return new Intl.NumberFormat( local, {
-        style:'currency',
-        currency:currencyCode
+    formatCurrency(amount, currencyCode, local = 'en-Us') {
+    return new Intl.NumberFormat(local, {
+        style: 'currency',
+        currency: currencyCode
     }).format(amount);
 }
 
 // Example
-console.log(formatCurrency(45478.568,'USD'));  //$45,478.57
-console.log(formatCurrency(45478.568,'EUR'));  //€45,478.57
-console.log(formatCurrency(45478.568,'EGP'));  //EGP 45,478.57
-console.log(formatCurrency(45478.568,'INR'));  //₹45,478.57
-console.log(formatCurrency(45478.568,'JPY'));  //¥45,479
-console.log(formatCurrency(45478.568,'SAR'));  //SAR 45,478.57
+console.log(formatCurrency(45478.568, 'USD'));  //$45,478.57
+console.log(formatCurrency(45478.568, 'EUR'));  //€45,478.57
+console.log(formatCurrency(45478.568, 'EGP'));  //EGP 45,478.57
+console.log(formatCurrency(45478.568, 'INR'));  //₹45,478.57
+console.log(formatCurrency(45478.568, 'JPY'));  //¥45,479
+console.log(formatCurrency(45478.568, 'SAR'));  //SAR 45,478.57
 
 // product free API
 fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            // product category free API 
+    .then(res => res.json())
+    .then(json => console.log(json))
+// product category free API 
 fetch('https://fakestoreapi.com/products/categories')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            // cart of user free API
+    .then(res => res.json())
+    .then(json => console.log(json))
+// cart of user free API
 fetch('https://fakestoreapi.com/carts?userId=1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            //limited product free API
+    .then(res => res.json())
+    .then(json => console.log(json))
+//limited product free API
 fetch('https://fakestoreapi.com/products?limit=5')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
+    .then(res => res.json())
+    .then(json => console.log(json))
 // random user APi
 fetch('https://randomuser.me/api/')
-            .then(res=>res.json())
-            .then(json=>console.log(json.results[0]))
-            console.log('location')
+    .then(res => res.json())
+    .then(json => console.log(json.results[0]))
+console.log('location')
 /* The code is checking if the browser supports geolocation by checking if
 `window.navigator.geolocation` is truthy. If geolocation is supported, it calls the
 `getCurrentPosition` method to get the current position of the user. The position object contains
 the latitude and longitude coordinates, which are then logged to the console. If there is an error
 retrieving the position, the error is logged to the console as well. */
-if(window.navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(position=>{
-        const{latitude,longitude}=position.coords;
-        console.log(latitude,longitude);
-        fetchLocationName(latitude,longitude)
-    },err=>{
+if (window.navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+        fetchLocationName(latitude, longitude)
+    }, err => {
         console.log(err);
     })
 }
